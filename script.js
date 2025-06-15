d3e8a7d67ebf58071f8a30863051518321cc3a03
Kashish Kiraula <kashishkiraula2410@gmail.com>
2025-06-15 13:40:19 +0530

    commit files

---
+++ script.js
@@ -0,0 +1,11 @@
+function openSchemesWindow() {
+    window.open("schemes.html", "_top","Schemes", "width=600,height=600");
+}
+
+function openMarketplaceWindow() {
+    window.open("marketplace.html","_top", "Marketplace", "width=800,height=600");
+}
+
+function applyForLoan() {
+    window.open("loans.html","_top", "Loans", "width=600,height=600");
+}
